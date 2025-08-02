38c43ea93e4f9917c013fd66d64cf634b1858af1
Pavel Popkov <goil2011@yandex.ru>
2025-08-02 15:58:51 +0300

    fix image

--- src/components/Empty/Empty.jsx
+++ src/components/Empty/Empty.jsx
@@ -1,13 +1,10 @@
 import styles from "./Empty.module.scss";
+import emptyImage from "../../../public/empty.png";
 
 export default function Empty() {
   return (
     <div className={styles.empty}>
-      <img
-        src="../public/empty.png"
-        alt="empty-image"
-        className={styles.empty__image}
-      />
+      <img src={emptyImage} alt="empty-image" className={styles.empty__image} />
       <h3 className={styles.empty__text}>Empty...</h3>
     </div>
   );
